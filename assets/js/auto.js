b2a6426fc9d0ea7aab9867ede1aeaf551fffe262
Igor <41655195+andzheyevskiy@users.noreply.github.com>
2025-01-09 11:02:16 +0100

    fix: general improvements

--- assets/js/auto.js
+++ assets/js/auto.js
@@ -7,22 +7,28 @@ async function ajax(callback) {
     return data
 }
 
-ajax((e) => {
+async function productos() {
     let resultadoHMTL = ""
-    e.forEach(categoria => {
-        resultadoHMTL += `<article>
+    await ajax((e) => {
+        e.forEach(categoria => {
+            resultadoHMTL += `<article>
+        <div class="carousell ${categoria.class}"></div>
         <h2>${categoria.nombre}</h2>
         ${categoria.descripcion ? `<p>${categoria.descripcion}</p>` : ""}
         <div class="contenedor">`
-        categoria.platos.forEach(plato => {
-            resultadoHMTL += `<div>
+
+            categoria.platos.forEach(plato => {
+                resultadoHMTL += `<div>
             <h3>${plato.nombre}</h3>
             ${plato.descripcion ? `<p>${plato.descripcion}</p>` : ""}
             <p class="precio">${plato.precio}€</p>
             </div>`
-        })
+            })
 
-        resultadoHMTL += "</div></article>"
-    });
-    console.log(resultadoHMTL)
-})
+            resultadoHMTL += "</div></article>"
+        });
+    })
+    return resultadoHMTL
+}
+
+productos().then(e => carta.innerHTML = e)
